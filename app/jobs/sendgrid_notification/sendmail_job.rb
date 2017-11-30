6380b7156968d2517868e6e8a7b447ba3e9542de
Yoshiyuki Hirano <yhirano@me.com>
2017-11-30 19:11:00 +0900

    Modify SendmailJob to set mail_from, mail_from_name

--- app/jobs/sendgrid_notification/sendmail_job.rb
+++ app/jobs/sendgrid_notification/sendmail_job.rb
@@ -1,11 +1,11 @@
 module SendgridNotification
-  class SendmailJob < ActiveJob::Base
+  class SendmailJob < ApplicationJob
     queue_as :sendmail
 
     MailerClass = Rails.application.config.sendgrid_notification.mailer.constantize
 
     def perform(to, notification_mail, params)
-      MailerClass.new.sendmail(to, notification_mail, params)
+      MailerClass.new(params).sendmail(to, notification_mail, params)
     end
   end
 end
