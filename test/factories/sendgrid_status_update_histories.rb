d2f864be8d03c759f1d82b2bdb5fc5cc68f8b2a2
Yoshiyuki Hirano <yhirano@me.com>
2017-12-01 16:09:13 +0900

    Modify const name FactoryGirl => FactoryBot

--- test/factories/sendgrid_status_update_histories.rb
+++ test/factories/sendgrid_status_update_histories.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :sendgrid_status_update_history, class: 'SendgridNotification::SendgridStatusUpdateHistory' do
     start_time { 2.hour.ago.to_i }
     end_time { 1.hour.ago.to_i }
