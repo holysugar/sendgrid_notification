062677f7b45eeeb5acf47f05f8d7e624408adc1a
HORII Keima <holysugar@gmail.com>
2019-12-11 21:14:47 +0900

    Fix attachment hash keys to symbolize

--- app/jobs/sendgrid_notification/sendmail_job.rb
+++ app/jobs/sendgrid_notification/sendmail_job.rb
@@ -9,7 +9,7 @@ module SendgridNotification
     def perform(to, notification_mail, params, hash_attachments = [])
 
       mailer = MailerClass.new(params)
-      obj_attachments = hash_attachments.map{|hash| SendgridNotification::Attachment.wrap(hash) }
+      obj_attachments = hash_attachments.map{|hash| SendgridNotification::Attachment.wrap(hash.symbolize_keys) }
       mailer.sendmail(to, notification_mail, params, obj_attachments)
       raise HTTPException, mailer.last_result.body unless mailer.last_result_success?
 
